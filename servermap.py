4332cf4b039b3b1e6e7cb6f13388e183ef253cc9
GaoZe36 <77998208+GaoZe36@users.noreply.github.com>
2022-05-17 23:59:20 -0700

    Delete servermap.py

--- servermap.py
+++
@@ -1,5 +0,0 @@
-import engine.servermap
-
-class ServerMap(engine.servermap.ServerMap):
-    def triggerSayhello(self, trigger, sprite):
-        self.setSpriteSpeechText(sprite, f"Hello World!!!!!!!!")
